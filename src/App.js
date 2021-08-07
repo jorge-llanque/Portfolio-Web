a02bafd38bd981243f60b0e2ae28443635568287
jorge <georgellanque@gmail.com>
2021-08-07 16:42:25 -0400

    Add new features

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
+import {gsap, Expo} from 'gsap'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import About from './components/About';
 import Header from './components/Header';
@@ -7,11 +8,61 @@ import Skills from './components/Skills'
 import Projects from './components/Projects'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
+import './App.css'
+import logo from './logo-portfolio.svg'
+
+
 
 function App() {
+  let overlay = useRef(null);
+  let hola = useRef(null);
+
+
+  useEffect(()=>{
+    gsap.to(overlay,{
+      duration: 1.5,
+      delay: .1,
+      top: "0%",
+      ease: Expo.easeInOut
+    })
+
+    gsap.to(hola,{
+      duration: 1,
+      rotation: 360,
+      transformOrigin: "center center",
+      delay: .5
+    })
+    gsap.to(hola,{
+      duration: 1,
+      ease: "back.out(4)",
+      x: 10,
+      delay: 2
+    })
+    gsap.to(hola, {
+      duration: 1,
+      "--color-logo":"green",
+      yoyo:true,
+      repeat: 1,
+      delay: 3
+    })
+    gsap.to(hola,{
+      duration: 1,
+      display: "none",
+      delay: 4
+    })
+    gsap.to(overlay,{
+      duration: 1.5,
+      delay: 4.5,
+      top: "-100%",
+      ease: Expo.easeInOut
+    })
+  }, [])
+
   return (
-    <div className="App">
+    <div>
       <BrowserRouter>
+        <div className="Overlay" ref={(el)=>{overlay = el}}/>
+        <div className="prueba" ref={(el) => {hola = el}}>Hola</div>
         <Header />
         <Hero />
         <About/>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useRef, useEffect} from 'react'
 import Navbar from './Navbar'
 import logo from './../logo-portfolio.svg'
 import './styles/Header.css'
+import {gsap, Expo} from 'gsap'
 
 export default function Header() {
+  let header = useRef(null);
+
+  useEffect(()=> {
+    gsap.to(header, {
+      duration: 1,
+      delay: 5.5,
+      top: 0,
+      ease: Expo.easeInOut
+    })
+  }, [])
 
   const body = document.body;
   const scrollUp = "scroll-up";
@@ -34,7 +45,7 @@ export default function Header() {
   });
 
   return (
-    <div className="Header">
+    <div className="Header" ref={(el) => {header = el}}>
         <img src={logo} alt="logo" className="Logo__Header"/>
         <Navbar />
     </div>
