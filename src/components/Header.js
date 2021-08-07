b219d844dc62b9b7372c3e69b27b3b825e1824ae
jorge <georgellanque@gmail.com>
2021-08-07 03:19:11 -0400

    Add logo.svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Navbar from './Navbar'
-import logo from './../logo.png'
+import logo from './../logo-portfolio.svg'
 import './styles/Header.css'
 
 export default function Header() {
