1b957e522c0a66f09a7bef728aa16a3a067e2527
jorge <georgellanque@gmail.com>
2021-08-09 04:56:00 -0400

    Add contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,19 @@
 import React from 'react'
 import './styles/Contact.css'
+import { SiLinkedin, SiTwitter, SiGithub } from 'react-icons/si'
 
 export default function Contact () {
   return (
     <section className="Contact__Section">
-      <button >Get In Touch</button>
+        <h2>Contact Me</h2>
+        <div className="Contact__Links">
+          <a className="Contact-Email" href="mailto:georgellanque@gmail.com">Send me an Email</a>
+          <ul>
+            <li><SiLinkedin/></li>
+            <li><SiTwitter/></li>
+            <li><SiGithub/></li>
+          </ul>
+        </div>
     </section>
   )
 }
