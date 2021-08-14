5d92bdc1598ad09a0788e5fd005462127ffc373a
jorge <georgellanque@gmail.com>
2021-08-14 05:09:14 -0400

    Change build

--- src/hooks/useLocoScroll.js
+++ src/hooks/useLocoScroll.js
@@ -1,18 +1,18 @@
-import { useEffect } from 'react'
-import LocomotiveScroll from 'locomotive-scroll'
-import 'locomotive-scroll/src/locomotive-scroll.scss'
+// import { useEffect } from 'react'
+// import LocomotiveScroll from 'locomotive-scroll'
+// import 'locomotive-scroll/src/locomotive-scroll.scss'
 
-export default function useLocoScroll (start) {
-  useEffect(() => {
-    if (start) return
-    const scrollEl = document.querySelector('#main-container')
-    console.log(scrollEl)
-    // eslint-disable-next-line no-unused-vars
-    const locoScroll = new LocomotiveScroll({
-      el: scrollEl,
-      smooth: true,
-      multiplier: 1,
-      class: 'is-reveal'
-    })
-  }, [])
-}
+// export default function useLocoScroll (start) {
+//   useEffect(() => {
+//     if (start) return
+//     const scrollEl = document.querySelector('#main-container')
+//     console.log(scrollEl)
+//     // eslint-disable-next-line no-unused-vars
+//     const locoScroll = new LocomotiveScroll({
+//       el: scrollEl,
+//       smooth: true,
+//       multiplier: 1,
+//       class: 'is-reveal'
+//     })
+//   }, [])
+// }
