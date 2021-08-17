291ac68f713e49e591ed6cad471ad69b5a2b017b
jorge <georgellanque@gmail.com>
2021-08-17 18:17:03 -0400

    Add assets folder

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import './styles/Projects.css'
-import projectImage1 from './../project-image-1.PNG'
-import projectImage2 from './../project-image-2.PNG'
+import projectImage1 from './../assets/project-image-1.PNG'
+import projectImage2 from './../assets/project-image-2.PNG'
+import projectImage3 from './../assets/project-image-3.PNG'
+import projectImage4 from './../assets/project-image-4.PNG'
 import { IoCheckmarkCircleSharp } from 'react-icons/io5'
+import './styles/Projects.css'
 
 export default function Projects () {
   return (
@@ -43,7 +45,19 @@ export default function Projects () {
                 </div>
               </header>
               <div className="Thumbnail__Wrap">
-                  <img loading="lazy" src={projectImage1} alt="img"/>
+                  <img loading="lazy" src={projectImage3} alt="img"/>
+              </div>
+            </article>
+            <article className="Projects__Item">
+              <header className="Item__Title">
+              <IoCheckmarkCircleSharp className="Item__Tic"/>
+                <div className="Title-Techs">
+                <h2><a href="/">Nutrition Recipes</a></h2>
+                <span>React | JavaScript | CSS3</span>
+                </div>
+              </header>
+              <div className="Thumbnail__Wrap">
+                  <img loading="lazy" src={projectImage4} alt="img"/>
               </div>
             </article>
             </div>
