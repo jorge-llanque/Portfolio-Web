afdbbb2f3ae45158654303b1d65099f5d54bf3af
jorge <georgellanque@gmail.com>
2021-08-17 22:11:13 -0400

    Update urls

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,34 @@
 import React, { useState } from 'react'
 import { FiAlignRight } from 'react-icons/fi'
+import { useLocation } from 'wouter'
 import './styles/Navbar.css'
 
 export default function Navbar () {
   const [openNavbar, setopenNavbar] = useState(false)
+  // eslint-disable-next-line no-unused-vars
+  const [location, setLocation] = useLocation()
 
   const handleOpenNavbar = () => {
     setopenNavbar(!openNavbar)
     console.log(openNavbar)
   }
 
+  const handleChangePage = () => {
+    setopenNavbar(!openNavbar)
+  }
+
   return (
     <div>
       <FiAlignRight onClick={handleOpenNavbar} className="Navbar__Logo"/>
       <div className= "Navbar__Content">
-        <nav className= {openNavbar ? 'Navbar__Links Navbar__Active' : 'Navbar__Links'}>
+        <nav className= {openNavbar ? 'Navbar__Active' : 'Navbar__Links'}>
           <ul>
-            <li><a href="/#about" key="2">About</a></li>
-            <li><a href="/#skills" key="3">Skills</a></li>
-            <li><a href="/#projects" key="4">Projects</a></li>
-            <li><a href="/#experience" key="5">Experience</a></li>
-            <li><a href="/#contact" key="6">Contact</a></li>
-            <li><a className="resume-button" href='/resume.pdf' target="_blank">Resume</a></li>
+            <li><a onClick={handleChangePage} href="/#about" key="2">About</a></li>
+            <li><a onClick={handleChangePage} href="/#skills" key="3">Skills</a></li>
+            <li><a onClick={handleChangePage} href="/#projects" key="4">Projects</a></li>
+            <li><a onClick={handleChangePage} href="/#experience" key="5">Experience</a></li>
+            <li><a onClick={handleChangePage} href="/#contact" key="6">Contact</a></li>
+            <li><a onClick={handleChangePage} className="resume-button" href='/resume.pdf' target="_blank">Resume</a></li>
           </ul>
         </nav>
         <div className= {openNavbar ? 'Wrapper__Active' : 'Navbar__Wrapper'}
