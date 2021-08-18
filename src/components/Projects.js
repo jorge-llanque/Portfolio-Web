1265d59bf26edc614d3ae552ee8a9f45b7f82076
jorge <georgellanque@gmail.com>
2021-08-17 21:37:36 -0400

    Update Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
+import { gsap, Expo } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 import projectImage1 from './../assets/project-image-1.PNG'
 import projectImage2 from './../assets/project-image-2.PNG'
 import projectImage3 from './../assets/project-image-3.PNG'
@@ -6,60 +8,95 @@ import projectImage4 from './../assets/project-image-4.PNG'
 import { IoCheckmarkCircleSharp } from 'react-icons/io5'
 import './styles/Projects.css'
 
+gsap.registerPlugin(ScrollTrigger)
+
 export default function Projects () {
+  const titleProjects = useRef(null)
+  const paraProjects = useRef(null)
+  const listProjects = useRef(null)
+
+  useEffect(() => {
+    gsap.to(titleProjects.current, {
+      scrollTrigger: {
+        trigger: titleProjects.current,
+        start: 'top bottom'
+      },
+      duration: 1,
+      opacity: 1,
+      ease: Expo.easeInOut
+    })
+    gsap.to(paraProjects.current, {
+      scrollTrigger: {
+        trigger: paraProjects.current,
+        start: 'top bottom'
+      },
+      duration: 1,
+      opacity: 1,
+      ease: Expo.easeInOut
+    })
+    gsap.to(listProjects.current, {
+      scrollTrigger: {
+        trigger: listProjects.current,
+        start: 'top bottom'
+      },
+      duration: 1,
+      opacity: 1,
+      ease: Expo.easeInOut
+    })
+  }, [])
   return (
       <section id="projects" className="Projects__Section" >
-            <h2>My Projects</h2>
-            <p>I like to build pages, get up project, imagine squares and life cycles...</p>
-            <div className="Projects__List">
-            <article className="Projects__Item">
-              <header className="Item__Title">
+            <h2 ref={titleProjects} >My Projects</h2>
+            <p ref={paraProjects}>I like to build pages, get up project, imagine squares and life cycles...</p>
+            <div ref={listProjects} className="Projects__List">
+              <article className="Projects__Item">
+                <header className="Item__Title">
+                  <IoCheckmarkCircleSharp className="Item__Tic"/>
+                  <div className="Title-Techs">
+                  <h2><a href="/">Nutrition Recipes</a></h2>
+                  <span>React | JavaScript | CSS3</span>
+                  </div>
+                </header>
+                <div className="Thumbnail__Wrap">
+                    <img loading="lazy" src={projectImage1} alt="img"/>
+                </div>
+              </article>
+              <article className="Projects__Item">
+                <header className="Item__Title">
                 <IoCheckmarkCircleSharp className="Item__Tic"/>
-                <div className="Title-Techs">
-                <h2><a href="/">Nutrition Recipes</a></h2>
-                <span>React | JavaScript | CSS3</span>
+                  <div className="Title-Techs">
+                  <h2><a href="/">Favorite Videos</a></h2>
+                  <span>React | JavaScript | CSS3</span>
+                  </div>
+                </header>
+                <div className="Thumbnail__Wrap">
+                    <img loading="lazy" src={projectImage2} alt="img"/>
                 </div>
-              </header>
-              <div className="Thumbnail__Wrap">
-                  <img loading="lazy" src={projectImage1} alt="img"/>
-              </div>
-            </article>
-            <article className="Projects__Item">
-              <header className="Item__Title">
-              <IoCheckmarkCircleSharp className="Item__Tic"/>
-                <div className="Title-Techs">
-                <h2><a href="/">Favorite Videos</a></h2>
-                <span>React | JavaScript | CSS3</span>
+              </article>
+              <article className="Projects__Item">
+                <header className="Item__Title">
+                <IoCheckmarkCircleSharp className="Item__Tic"/>
+                  <div className="Title-Techs">
+                  <h2><a href="/">Nutrition Recipes</a></h2>
+                  <span>React | JavaScript | CSS3</span>
+                  </div>
+                </header>
+                <div className="Thumbnail__Wrap">
+                    <img loading="lazy" src={projectImage3} alt="img"/>
                 </div>
-              </header>
-              <div className="Thumbnail__Wrap">
-                  <img loading="lazy" src={projectImage2} alt="img"/>
-              </div>
-            </article>
-            <article className="Projects__Item">
-              <header className="Item__Title">
-              <IoCheckmarkCircleSharp className="Item__Tic"/>
-                <div className="Title-Techs">
-                <h2><a href="/">Nutrition Recipes</a></h2>
-                <span>React | JavaScript | CSS3</span>
+              </article>
+              <article className="Projects__Item">
+                <header className="Item__Title">
+                <IoCheckmarkCircleSharp className="Item__Tic"/>
+                  <div className="Title-Techs">
+                  <h2><a href="/">Nutrition Recipes</a></h2>
+                  <span>React | JavaScript | CSS3</span>
+                  </div>
+                </header>
+                <div className="Thumbnail__Wrap">
+                    <img loading="lazy" src={projectImage4} alt="img"/>
                 </div>
-              </header>
-              <div className="Thumbnail__Wrap">
-                  <img loading="lazy" src={projectImage3} alt="img"/>
-              </div>
-            </article>
-            <article className="Projects__Item">
-              <header className="Item__Title">
-              <IoCheckmarkCircleSharp className="Item__Tic"/>
-                <div className="Title-Techs">
-                <h2><a href="/">Nutrition Recipes</a></h2>
-                <span>React | JavaScript | CSS3</span>
-                </div>
-              </header>
-              <div className="Thumbnail__Wrap">
-                  <img loading="lazy" src={projectImage4} alt="img"/>
-              </div>
-            </article>
+              </article>
             </div>
       </section>
   )
